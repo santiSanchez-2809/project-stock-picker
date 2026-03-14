7741f76c203c4978842821ac1a014be0a9116e0a
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-14 18:37:48 -0300

    Add second version of stock_picker with O(n) time complexity (previous version had a complexity of O(n²)

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
-def stock_picker(stock_prices)
+def stock_picker(prices)
   max_profit = 0
   buying_day = 0
   selling_day = 0
 
-  stock_prices.each_with_index do |buying_price, day|
+  prices.each_with_index do |buying_price, day|
 
-    remaining_days = stock_prices[day, stock_prices.length - day]
+    remaining_days = prices[day, prices.length - day]
 
     max_profit_for_day = 0
     best_selling_day = 0
@@ -26,4 +26,30 @@ def stock_picker(stock_prices)
   end
 
   [buying_day, selling_day]
+end
+
+def stock_picker2(prices)
+  min_price = prices[0]
+  min_day = 0
+
+  max_profit = 0
+  buy_day = 0
+  sell_day = 0
+
+  prices.each_with_index |price, day|
+    profit = price - min_price
+
+    if profit > max_profit
+      max_profit = max_profit
+      buy_day = min_day
+      sell_day = day
+    end
+
+    if price < min_price
+      min_price = price
+      min_day = day
+    end
+  end
+
+  [buy_day, sell_day]
 end
